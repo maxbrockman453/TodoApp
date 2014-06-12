a8e31445b7a77efb1937890ca143703f51e6d20c
Max Brockman <maxbrockman453@gmail.com>
2014-06-12 12:07:03 -0700

    super big commit from everything we've on to make the Sass file structure perfect as well as adding js for time recognition and colors

--- js/main.js
+++ js/main.js
@@ -2,12 +2,26 @@
       var item = document.querySelector('#item'),
           form = document.querySelector('form'),
           list = document.querySelector('#list');
+      var date = new Date();
+      var now = date.getHours();
+      var time1 = 0;
+      var time2 = 12;
+      var time3 = 17;
         form.addEventListener('submit', function(ev) {
           if (list.classList.contains ("new")) {
           list.innerHTML += '<li class="new">' + item.value + '</li>';
         } else {
           list.innerHTML += '<li>' + item.value + '</li>';
         }
+         if (now > time1 && now < time2) {
+            li.setAttribute("class", "am")
+          }
+          else if (now > time2 && now < time3) {
+            li.setAttribute("class", "noon")
+          }
+          else {
+            li.setAttribute("class", "pm");
+          }
           store();
           ev.preventDefault();
         }, false);
